78f60338d23dadc6267efb7a8aab7372ccff5316
Vadim Zinchenko <vadim.zinchenco@mail.ru>
2022-07-14 05:39:37 +0000

    build project

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ export const modals = () => {
     function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelectorAll(closeSelector);
+            close = document.querySelectorAll(closeSelector),
+            scroll = calcScroll();
 
         trigger.forEach(elem => {
 
@@ -33,6 +34,20 @@ export const modals = () => {
         }, time);
     }
 
+    function calcScroll () {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
@@ -47,6 +62,7 @@ export function openModal(selector) {
     closeModal();
     selector.style.display = 'block';
     document.body.classList.add('modal-open');
+    document.body.style.marginRight = `${scroll}px`;
 }
 
 export function closeModal() {
@@ -55,4 +71,5 @@ export function closeModal() {
         item.style.display = 'none';
     });
     document.body.classList.remove('modal-open');
+    document.body.style.marginRight = `0px`;
 }
